0cfdcec9b82cb11a29896201caa091db8043a1d5
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-08-24 21:08:40 +0300

    finish

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import contactsRouter from './routes/contacts.js';
+import authRouter from './routes/auth.js'; 
+
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
@@ -15,10 +18,15 @@ export function setupServer() {
   app.use(cors());
   app.use(morgan('dev'));
   app.use(express.json());
+  app.use(cookieParser());
 
+  // Auth rotaları
+  app.use('/auth', authRouter); 
+
+  // Contacts rotaları
   app.use('/contacts', contactsRouter);
 
-  
+  // Not found & error handler
   app.use(notFoundHandler);
   app.use(errorHandler);
 
